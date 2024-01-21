3eb73a35b67519a3d559b23acd1666c802d04a5d
I526525 <aryan.saxena@sap.com>
2024-01-22 02:11:47 +0530

    Day 1

---
+++ 30 day of code LeetCode questions/day_1.js
@@ -0,0 +1,10 @@
+// Airthematic Operators
+//1. Write a program to add two numbers without using the `+` operator
+
+const sum = (a,b) =>{
+
+   return a + b;
+}
+
+console.log("1. Sum = " + sum(20,30));
+
