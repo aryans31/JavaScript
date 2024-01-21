4c6c39cb806a010ecadb6febe847e2cabad17e96
I526525 <aryan.saxena@sap.com>
2024-01-22 02:06:26 +0530

    initial commit

---
+++ 30 day of code LeetCode questions/helloWorld.js
@@ -0,0 +1,23 @@
+
+
+// print hello world using functions
+
+
+ function createHelloWorld()
+{
+    return function (... args){
+        
+        return "Hello World";
+    }
+}
+console.log(createHelloWorld()());
+console.log(sum(2,3));
+
+// Basic operators
+
+const sum = (x,y) => {
+    let a,b;
+    return a+b;
+}
+
+
